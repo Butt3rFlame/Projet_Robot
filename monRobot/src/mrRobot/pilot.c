88b9be2ff1921200cea2e64df16000594e4e7e2f
Matthieu <matthieu.bonhomme@reseau.eseo.fr>
2023-02-03 12:49:11 +0100

    MAE table version

--- monRobot/src/mrRobot/pilot.c
+++ monRobot/src/mrRobot/pilot.c
@@ -10,21 +10,45 @@ typedef struct{
 } Pilot;
 
 typedef enum{
-    IDLE=0,
-    RUNNING
-} MachineState;
+    S_NONE = 0,
+    S_IDLE,
+    S_RUNNING,
+    NB_STATE
+} State;
 
 typedef enum{
-    EVENT_SET_VELOCITY,
-    EVENT_CHECK,
+    E_SET_VELOCITY=0,
+    E_CHECK,
+    E_STOP,
+    NB_EVENT
 } Event;
 
-static MachineState machineState;
+typedef enum{
+    A_NONE,
+    A_SEND_MVT,
+    A_CHECK
+} Action;
+
+typedef struct{
+    State nextState;
+    Action action;
+} Transition;
+
+static State currentState;
 static Event event;
 static Pilot *pilot;
 
+static Transition stateMachine[NB_STATE][NB_EVENT] = {
+    [S_IDLE][E_STOP] = {S_IDLE, A_SEND_MVT},
+    [S_IDLE][E_SET_VELOCITY] = {S_RUNNING, A_SEND_MVT},
+
+    [S_RUNNING][E_SET_VELOCITY] = {S_RUNNING, A_SEND_MVT},
+    [S_RUNNING][E_CHECK] = {S_RUNNING, A_CHECK}
+};
 
 static void run(Event event, VelocityVector vel);
+
+static void performAction(Action action);
 
 static void sendMvt(VelocityVector vel);
 
@@ -34,13 +58,13 @@ static bool hasBumped();
 extern void Pilot_start()
 {
     Robot_start();
-    machineState = IDLE;
+    currentState = S_IDLE;
 }
 
 extern void Pilot_stop()
 {
     Robot_stop();
-    machineState = IDLE;
+    currentState = S_IDLE;
 }
 
 extern void Pilot_new()
@@ -57,7 +81,7 @@ extern void Pilot_free()
 
 extern void Pilot_setVelocity(VelocityVector vel)
 {
-    event = EVENT_SET_VELOCITY;
+    event = E_SET_VELOCITY;
     pilot->vel = vel;
     run(event, vel);
 }
@@ -71,42 +95,41 @@ void Pilot_check()
 {
     pilot->state.collision = hasBumped();
     pilot->state.speed = Robot_getRobotSpeed();
-    event = EVENT_CHECK;
-    run(event, pilot->vel);
+    if (hasBumped()){
+        pilot->vel.dir = STOP;
+        pilot->vel.power = 0;
+        currentState = S_IDLE;
+        run(E_SET_VELOCITY, pilot->vel);
+    }
 }
 
 static void run(Event event, VelocityVector vel)
 {
-    switch (machineState){
-        default:
-        case IDLE:
-            if(event == EVENT_SET_VELOCITY){
-                if(vel.dir == STOP || pilot->state.collision){
-                    Pilot_stop();
-                }
-                else{
-                    machineState = RUNNING;
-                }
-            }
-        case RUNNING:
-            if(event == EVENT_SET_VELOCITY){
-                if(vel.dir == STOP){
-                    machineState = IDLE;
-                }
-                else{
-                    sendMvt(vel);
-                }
-            }
-            else if(event == EVENT_CHECK){
-                if(pilot->state.collision){
-                    Pilot_stop();
-                    machineState = IDLE;
-                }
-            }
+    Action action;
+    action = stateMachine[currentState][event].action;
+    State nextState = stateMachine[currentState][event].nextState;
+
+    if(nextState != S_NONE){
+        currentState = nextState;
+        performAction(action);
     }
 }
 
-void sendMvt(VelocityVector vel)
+static void performAction(Action action)
+{
+    switch(action){
+        case A_NONE:
+            break;
+        case A_CHECK:
+            Pilot_check();
+            break;
+        case A_SEND_MVT:
+            sendMvt(pilot->vel);
+            break;
+    }
+}
+
+static void sendMvt(VelocityVector vel)
 {
     switch(vel.dir){
 
@@ -126,8 +149,9 @@ void sendMvt(VelocityVector vel)
             Robot_setWheelsVelocity(-vel.power, -vel.power);
             break;
 
-        default:
+        case STOP:
             Pilot_stop();
+            currentState = S_IDLE;
             break;
     }
 }
